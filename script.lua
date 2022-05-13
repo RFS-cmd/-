dfced607d3641664a28fd841e7138500a5c587ae
RFS-cmd <76849500+RFS-cmd@users.noreply.github.com>
2022-05-12 23:43:55 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 getgenv().Commands = {}
-getgenv().Prefix = "!" -- ONLY space out something like /e so its not like "/egoto <plrname>", Can be a word of your choice for example: ".goto <plrname>"
+getgenv().Prefix = "$" -- ONLY space out something like /e so its not like "/egoto <plrname>", Can be a word of your choice for example: ".goto <plrname>"
 getgenv().AddCommand = function(CmdName, CmdAlias, Desc, Func)
    Commands[#Commands + 1] = {
        ["Name"] = CmdName;
@@ -53,109 +53,106 @@ AddCommand("ws", {"walkspeed", "speed"}, "Sets Your WalkSpeed", function(args)
    Notify("zxciaz", "WalkSpeed: "..tonumber(args[1]), "", 3) -- Notifies you of the command or whatever
 end)
 
-AddCommand("kill", {"death", "vkill"}, "Teleports your to a player", function(args)
+AddCommand("kill", {"murder", "vkill"}, "Killa player", function(args)
    if args[1] then
        local Target = psearch(args[1]) -- Im giving the player finder as a bonus lol
-       Notify("zxciaz", "Teleporting To: "..tostring(Target), "", 3)
-local lp = game:GetService("Players").LocalPlayer
- 
-local function gplr(String)
-	local Found = {}
-	local strl = String:lower()
-	if strl == "all" then
-		for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-			table.insert(Found,v)
-		end
-	elseif strl == "others" then
-		for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-			if v.Name ~= lp.Name then
-				table.insert(Found,v)
-			end
-		end 
-	elseif strl == "me" then
-		for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-			if v.Name == lp.Name then
-				table.insert(Found,v)
-			end
-		end 
-	else
-		for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-			if v.Name:lower():sub(1, #String) == String:lower() then
-				table.insert(Found,v)
-			end
-		end 
-	end
-	return Found 
+       Notify("zxciaz", "Killing : "..tostring(Target), "", 3)
+       if Target and Target.Character then
+           local plr = game.Players.LocalPlayer
+local Target = plr.Name
+local Player = function(Ev)
+ if Ev == "" then
+  return nil
+ elseif Ev == "random" then
+  return game:GetService("Players"):GetPlayers()[math.random(1, #game:GetService("Players"):GetPlayers())]
+ else
+  for _, v in pairs(game:GetService("Players"):GetPlayers()) do
+   if v.Name:lower():sub(1, #Ev) == Ev:lower() or v.DisplayName:lower():sub(1, #Ev) == Ev then
+    return v
 end
+end
+end
+end
+local timeLeft = 3
+while true do
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players[Target].Character.HumanoidRootPart.CFrame
+timeLeft = timeLeft - 1
+wait(1)
+if  timeLeft == 0 then
+print("script breaked")
+ break
+end
+end
+local plr = game.Players.LocalPlayer
+ local Character = workspace:WaitForChild(plr.Name)
+ plr.Character.Humanoid:UnequipTools()
+ local Humanoid = plr.Character.Humanoid:Clone()
+ local Tool = plr.Backpack:FindFirstChildOfClass("Tool")
 
-	local Player = gplr(Target)
-	if Player[1] then
-		Player = Player[1]
-          LocalPlayer = game.Players.LocalPlayer
-          
-         if LocalPlayer.Character.PrimaryPart ~= nil then
-                local Character = LocalPlayer.Character
-                local previous = LocalPlayer.Character.PrimaryPart.CFrame
-                
-                Character.Archivable = true
-                local Clone = Character:Clone()
-                LocalPlayer.Character = Clone
-                wait(0.5)
-                LocalPlayer.Character = Character
-                wait(0.2)
-                
-                if LocalPlayer.Character and Player.Character and Player.Character.PrimaryPart ~= nil then
-                    if LocalPlayer.Character:FindFirstChildOfClass("Humanoid") then
-                        LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):Destroy()
-                    end
-    
-                    local Humanoid = Instance.new("Humanoid")
-                    Humanoid.Parent = LocalPlayer.Character
-    
-                    local Tool = nil
-    
-                    if LocalPlayer.Character:FindFirstChildOfClass("Tool") then
-                        Tool = LocalPlayer.Character:FindFirstChildOfClass("Tool")
-                    elseif LocalPlayer.Backpack and LocalPlayer.Backpack:FindFirstChildOfClass("Tool") then
-                        Tool = LocalPlayer.Backpack:FindFirstChildOfClass("Tool")
-                    end
-                    print(Tool)
+ plr.Character.Animate.Disabled = true
+ plr.Character.Humanoid:Destroy()
+ Humanoid.Parent = plr.Character
+ Tool.Parent = plr.Character
+    game.Players.LocalPlayer.Character.Humanoid:ChangeState(15)
+ firetouchinterest(Target.Character.HumanoidRootPart, Tool.Handle, 0);
 
-                    if Tool ~= nil then
-                        Tool.Parent = LocalPlayer.Backpack
-                        
-                        Player.Character.HumanoidRootPart.Anchored = true
-                        
-                        local Arm = game.Players.LocalPlayer.Character['Right Arm'].CFrame * CFrame.new(0, -1, 0, 1, 0, 0, 0, 0, 1, 0, -1, 0)
-                        Tool.Grip = Arm:ToObjectSpace(Player.Character.PrimaryPart.CFrame):Inverse()
-                        
-                        Tool.Parent = LocalPlayer.Character
-                        Workspace.CurrentCamera.CameraSubject = Tool.Handle
-              
-                        repeat
-                            wait()
-                        until not Tool or Tool and (Tool.Parent == Workspace or Tool.Parent == Player.Character)
-                        Player.Character.HumanoidRootPart.Anchored = false
-                        wait(0.1)
-                        Humanoid.Health = 0
-                        LocalPlayer.Character = nil
-                    end
-                end
-     
-                spawn(function()
-                LocalPlayer.CharacterAdded:Wait()
-                Player.Character.HumanoidRootPart.Anchored = false
-                if Player.Character.Humanoid.Health <= 15 then
-                 notif("The requested user has been killed!")
-                repeat wait() until LocalPlayer.Character.PrimaryPart ~= nil
-                wait(0.4)
-                LocalPlayer.Character:SetPrimaryPartCFrame(previous)
-              end
-           end)
-       end
-    end
+
+
+
+    plr.Character.Humanoid:UnequipTools()
+    local Humanoid = plr.Character.Humanoid:Clone()
+    local Tool = plr.Backpack:FindFirstChildOfClass("Tool")
+
+    plr.Character.Animate.Disabled = true
+    plr.Character.Humanoid:Destroy()
+    Humanoid.Parent = plr.Character
+    Tool.Parent = plr.Character
+    firetouchinterest(Target.Character.HumanoidRootPart, Tool.Handle, 0); wait(0.1)
+    plr.Character.Humanoid.Health = 0
+    plr.Character = nil
+  
+   
+   plr.CharacterAdded:Wait()
+   plr.Character:WaitForChild("Humanoid")
+
+
+ local plr = game.Players.LocalPlayer
+ local Character = workspace:WaitForChild(plr.Name)
+ plr.Character.Humanoid:UnequipTools()
+ local Humanoid = plr.Character.Humanoid:Clone()
+ local Target = Player(Target)
+ local Tool = plr.Backpack:FindFirstChildOfClass("Tool")
+
+ plr.Character.Animate.Disabled = true
+ plr.Character.Humanoid:Destroy()
+ Humanoid.Parent = plr.Character
+ Tool.Parent = plr.Character
+    game.Players.LocalPlayer.Character.Humanoid:ChangeState(15)
+ firetouchinterest(Target.Character.HumanoidRootPart, Tool.Handle, 0);
+
+
+
+
+    plr.Character.Humanoid:UnequipTools()
+    local Humanoid = plr.Character.Humanoid:Clone()
+    local Tool = plr.Backpack:FindFirstChildOfClass("Tool")
+
+    plr.Character.Animate.Disabled = true
+    plr.Character.Humanoid:Destroy()
+    Humanoid.Parent = plr.Character
+    Tool.Parent = plr.Character
+    firetouchinterest(Target.Character.HumanoidRootPart, Tool.Handle, 0); wait(0.1)
+    plr.Character.Humanoid.Health = 0
+    plr.Character = nil
+  
+   
+   plr.CharacterAdded:Wait()
+   plr.Character:WaitForChild("Humanoid")
+
+
+end
+end
 end)
-
 -- Checks if you chatted
 LocalP.Chatted:Connect(function(Msg)
    CheckCmd(Msg)
